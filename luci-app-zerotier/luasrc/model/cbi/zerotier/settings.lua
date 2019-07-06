505a177dce9203aded7ac4b00744581025da3464
a520ass <a520ass@hotmail.com>
2019-07-06 17:35:18 +0800

    zerotier移除nat功能

--- luci-app-zerotier/luasrc/model/cbi/zerotier/settings.lua
+++ luci-app-zerotier/luasrc/model/cbi/zerotier/settings.lua
@@ -14,13 +14,5 @@ e=t:option(DynamicList,"join",translate('ZeroTier Network ID'))
 e.password=true
 e.rmempty=false
 
-e=t:option(Flag,"nat",translate("Auto NAT Clients"))
-e.default=0
-e.rmempty=false
-e.description = translate("Allow zerotier clients access your LAN network")
-
-e=t:option(DummyValue,"opennewwindow" , 
-	translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"Zerotier.com\" onclick=\"window.open('https://my.zerotier.com/network')\" />"))
-e.description = translate("Create or manage your zerotier network, and auth clients who could access")
 
 return a
