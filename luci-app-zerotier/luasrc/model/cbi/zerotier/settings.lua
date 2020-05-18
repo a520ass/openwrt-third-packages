5326b9977e1815caabb0a27f8139bfe820d010d3
hefeng <a520ass@hotmail.com>
2020-05-18 11:37:22 +0800

    update lean

--- luci-app-zerotier/luasrc/model/cbi/zerotier/settings.lua
+++ luci-app-zerotier/luasrc/model/cbi/zerotier/settings.lua
@@ -10,16 +10,17 @@ e=t:option(Flag,"enabled",translate("Enable"))
 e.default=0
 e.rmempty=false
 
-e=t:option(Value,"secret",translate("Secret"))
-e.password=true
-
 e=t:option(DynamicList,"join",translate('ZeroTier Network ID'))
 e.password=true
 e.rmempty=false
 
-local apply = luci.http.formvalue("cbi.apply")
-if apply then
-	io.popen("/etc/init.d/zerotier restart")
-end
+e=t:option(Flag,"nat",translate("Auto NAT Clients"))
+e.default=0
+e.rmempty=false
+e.description = translate("Allow zerotier clients access your LAN network")
+
+e=t:option(DummyValue,"opennewwindow" , 
+	translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"Zerotier.com\" onclick=\"window.open('https://my.zerotier.com/network')\" />"))
+e.description = translate("Create or manage your zerotier network, and auth clients who could access")
 
 return a
